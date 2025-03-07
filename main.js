bb465ccbc909c00f0fbc8733eb229e69a3841236
MuhammadFarid956 <mhmmdfaridirfn@gmail.com>
2025-03-07 08:27:13 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,3 @@
-// document.addEventListener("DOMContentLoaded", function() {
-//     let links= document.querySelectorAll(".navbar .navbar-nav a");
-//     let currentUrl= window.location.pathname.split("/").pop("");
-
-//     links.forEach(link=> {
-//         if (link.getAttribute("href") === currenturl) {
-//             link.classList.add("active");
-//         }
-//     });
-// });
-
 //  !Toggle Class Active
 const navbarNav = document.querySelector(".navbar-nav");
 document.querySelector(`#hamburger-menu`).onclick = () => {
@@ -18,8 +7,8 @@ document.querySelector(`#hamburger-menu`).onclick = () => {
 // !Klik diluar sidebar untuk menghilangkan nav
 const hamburger = document.querySelector(`#hamburger-menu`);
 
-document.addEventListener("click", function(e) {
+document.addEventListener("click", function (e) {
   if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
   }
-})
+});
